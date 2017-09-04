b7370ec2b1a54515e403f2d8a42a6bae5e66271b
Brian <bcfuchs@gmail.com>
2017-09-04 20:16:15 +0100

    language

--- uredb2edm.groovy
+++ uredb2edm.groovy
@@ -418,7 +418,14 @@ class Edm {
     }
     def prefix() {
 return '''
-<rdf:RDF xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:edm="http://www.europeana.eu/schemas/edm/" xmlns:wgs84_pos="http://www.w3.org/2003/01/geo/wgs84_pos#" xmlns:foaf="http://xmlns.com/foaf/0.1/" xmlns:rdaGr2="http://rdvocab.info/ElementsGr2/" xmlns:oai="http://www.openarchives.org/OAI/2.0/" xmlns:owl="http://www.w3.org/2002/07/owl#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:ore="http://www.openarchives.org/ore/terms/" xmlns:skos="http://www.w3.org/2004/02/skos/core#" xmlns:dcterms="http://purl.org/dc/terms/">
+<rdf:RDF xmlns:dc="http://purl.org/dc/elements/1.1/" 
+         xmlns:dcterms="http://purl.org/dc/terms/"
+         xmlns:edm="http://www.europeana.eu/schemas/edm/" 
+         xmlns:wgs84_pos="http://www.w3.org/2003/01/geo/wgs84_pos#" 
+         xmlns:oai="http://www.openarchives.org/OAI/2.0/" 
+         xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" 
+         xmlns:ore="http://www.openarchives.org/ore/terms/" 
+         xmlns:skos="http://www.w3.org/2004/02/skos/core#">
 '''
 
 
